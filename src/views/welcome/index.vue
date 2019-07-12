c2c8632b6ddd9a0745d030f7a940ffbf266cac32
lg <1378293285@qq.com>
2019-07-12 18:00:18 +0800

    首页布局完成

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="welcome-container">
+        <img src="../../assets/images/welcome.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.welcome-container {
+    img {
+        transform: translate(60%)
+    }
+}
+</style>
